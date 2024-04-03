1ee6ac6d8f775ab69ef81584a98427981116c8f3
J7ohn <jvfonseca1349@gmail.com>
2024-04-03 17:05:08 -0300

    Começando a engatinhar no desenvolvimento de um app android

---
+++ app/src/main/java/com/example/applistavip/model/Pessoa.java
@@ -0,0 +1,24 @@
+package com.example.applistavip.model;
+
+public class Pessoa {
+
+
+    private String primeiroNome;
+    public String segundoNome;
+    public String nomeCurso;
+    public int tel;
+
+    public Pessoa (String primeiroNome, String segundoNome, String nomeCurso, int tel) {
+
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+        this.nomeCurso = nomeCurso;
+        this.tel = tel;
+    }
+
+    public Pessoa(){
+
+    }
+
+
+}
