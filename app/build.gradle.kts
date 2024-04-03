e68e5bd23edec2548df52c49c1f9a5d619772289
J7ohn <jvfonseca1349@gmail.com>
2024-04-03 00:04:22 -0300

    Algumas axclusoes de arquivos de teste segundo a aula

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
     }
 
     buildTypes {
@@ -34,7 +33,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
